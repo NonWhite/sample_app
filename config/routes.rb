e96268d340927c85b274d6cec39e9af80587e511
NonWhite <wperezurcia@gmail.com>
2013-11-30 16:36:39 -0500

    Added Mision/Vision pages

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ SampleApp::Application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/services', to: 'static_pages#services', via: 'get'
   match '/minery_services', to: 'static_pages#minery_services', via: 'get'
+  match '/mision', to: 'static_pages#mision', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
